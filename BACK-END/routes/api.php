f7cad4326c7187614ca17057ae61596853ae30db
Emanuel Lima <spryler@hotmail.com>
2023-08-05 15:53:42 -0300

    First commit

--- BACK-END/routes/api.php
+++ BACK-END/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// User CRUD;
+//  User CRUD;
 Route::post('registro', [\App\Http\Controllers\UserController::class, 'register']);
 Route::post('login', [\App\Http\Controllers\UserController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
